a1114a0c548bedea4a64f36fede7ba38ed455ac1
victormagallanes2 <victormagallanes2@gmail.com>
2016-07-18 10:41:08 -0400

    Actualización de logs

--- circon/configuration/logs/urls.py
+++ circon/configuration/logs/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, url
 from .views import ListLogs
+from .views import LogsProducts
 
 
-urlpatterns = patterns('', url(r'^ListLogs$', ListLogs.as_view(),
-                               name='list_logs'),
+urlpatterns = patterns('',
+                       url(r'^ListLogs$', ListLogs.as_view(),
+                           name='list_logs'),
+                       url(r'^LogsProducts$', LogsProducts.as_view(),
+                           name='logs_products'),
                        )
--- circon/configuration/logs/views.py
+++ circon/configuration/logs/views.py
@@ -1,9 +1,15 @@
 from django.views.generic import ListView
+from django.views.generic import TemplateView
 from pure_pagination.mixins import PaginationMixin
 from circon.warehouse.products.models import Products
 
 
-class ListLogs(PaginationMixin, ListView):
+class ListLogs(PaginationMixin, TemplateView):
+    template_name = 'configuration/logs/tables.html'
+    paginate_by = 10
+
+
+class LogsProducts(PaginationMixin, ListView):
     template_name = 'configuration/logs/list.html'
     queryset = Products.audit_log.all()
     paginate_by = 10
