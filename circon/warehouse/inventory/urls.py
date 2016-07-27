bc8ca2c415f09c4aed48e8653a0f1035e5409270
victormagallanes2 <victormagallanes2@gmail.com>
2016-07-27 15:07:30 -0400

    Boton imprimir

--- circon/warehouse/inventory/urls.py
+++ circon/warehouse/inventory/urls.py
@@ -6,6 +6,6 @@ from .views import SearchCategory
 urlpatterns = patterns(' ',
                        url(r'^List_Inventory$', Inventory.as_view(),
                            name='list_inventory'),
-                       url(r'^/SearchCategory/(?P<pk>[0-9]+)/$', SearchCategory.as_view(),
+                       url(r'^SearchCategory/(?P<pk>[0-9]+)/$', SearchCategory.as_view(),
                            name='search_category'),
                        )
